1d46ccb98074b18d39017bd981e5f2fc655679e5
kazuhakun16 <50296105+kazuhakun16@users.noreply.github.com>
2019-05-05 22:01:13 +0800

    Add files via upload

---
+++ Test web/scripts/main.js
@@ -0,0 +1,17 @@
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'FGO SO AMPASS, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'FGO SO AMPASS, ' + storedName;
+}
+myButton.onclick = function() {
+setUserName();
+}
